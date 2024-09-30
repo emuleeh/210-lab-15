6a760d025584662df0e279eb6258e73a10d3dd7d
Emily <emuleehw@gmail.com>
2024-09-29 21:32:05 -0700

    Second commit

--- 210-lab-15/main.cpp
+++ 210-lab-15/main.cpp
@@ -1,3 +1,19 @@
 //COMSC 210 | Lab 15 | Em
 #include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
+using namespace std;
 
+class Movie {
+private:
+    string title;
+    int year;
+    string writer;
+public:
+    //getters and setters
+    string getTitle()           { return title; }
+    void setTitle(string t)     { title = t; }
+    int getYear()               { return year; }
+    void setYear(int y)         { year = y; }
+}
