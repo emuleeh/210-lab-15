ba167cfa8b540915c72c3776302b2014bcdcc1c2
Emily <emuleehw@gmail.com>
2024-09-29 21:26:58 -0700

    Initial Commit

--- 210-lab-15/main.cpp
+++ 210-lab-15/main.cpp
@@ -1,15 +1,3 @@
-//
-//  main.cpp
-//  210-lab-15
-//
-//  Created by E on 9/29/24.
-//  Copyright © 2024 E. All rights reserved.
-//
-
+//COMSC 210 | Lab 15 | Em
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
