370590d3f7f193db869459fb9aa48641e1a4e8bf
Emily <emuleehw@gmail.com>
2024-09-29 21:35:12 -0700

    Third commit

--- 210-lab-15/main.cpp
+++ 210-lab-15/main.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 using namespace std;
 
+const int W15 = 15;
+
 class Movie {
 private:
     string title;
@@ -16,4 +18,17 @@ public:
     void setTitle(string t)     { title = t; }
     int getYear()               { return year; }
     void setYear(int y)         { year = y; }
+    string getWriter()          { return writer; }
+    void setWriter(string w)    { writer = w; }
+    
+    //print method
+    void print() {
+        cout << setw(W15) << "Movie: " << title << endl;
+        cout << setw(W15) << "Year released: " << year << endl;
+        cout << setw(W15) << "Screenwriter: " << writer << endl;
+    }
+};
+
+int main() {
+    
 }
